2ca70704315f8ed9a515c98a192c58db9bcf3fbb
jodieeagleson <jodieeagleson@gmail.com>
2021-08-31 16:10:11 -0400

    updated content

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const includeHTML =() => {
-  const z, i, elmnt, file, xhttp;
+const includeHTML = () => {
+  // const z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
